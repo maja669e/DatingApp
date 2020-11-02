db94ff2990a5ab130daab3ac435a0f4e957ebed8
Maja <maja669e@stud.kea.dk>
2020-11-03 00:56:10 +0100

    added html filer og lavet styling på index

---
+++ dating_app/src/main/resources/static/script.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", burgerMenu);
+var burger = document.getElementById("myTopnav");
+
+
+function burgerMenu() {
+    if (burger.className === "topnav") {
+        burger.className += " responsive";
+    } else {
+        burger.className = "topnav";
+    }
+}
