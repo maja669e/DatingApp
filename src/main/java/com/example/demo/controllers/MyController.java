5ea56d6e37f8e87b50685b84374b8d3bb3c5aec1
NicolaiOkkels <nico902t@stud.kea.dk>
2020-11-04 20:42:30 +0100

    made profile html

--- src/main/java/com/example/demo/controllers/MyController.java
+++ src/main/java/com/example/demo/controllers/MyController.java
@@ -33,8 +33,13 @@ public class MyController {
 
     @GetMapping("/profil")
     public String profile(Model model){
-        DatingUser datingUser = new DatingUser("Nicolai Okkels", "bla bla bla bla", LocalDate.of(1996,2,1), 20959300, "Mand", "blablabla@gmail.com", "010296");
+        DatingUser datingUser = new DatingUser("bla bla", "bla bla bla bla", LocalDate.of(1996,2,1), 88888888, "Mand", "blablabla@gmail.com", "1");
         model.addAttribute("datingUser", datingUser); //Temp to test
         return "profil";
+    }
+
+    @GetMapping("/rediger")
+    public String edit(){
+        return "rediger";
     }
 }
