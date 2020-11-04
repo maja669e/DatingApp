a50467063ce82ffd2bfc9b4e779c37f9d6053f03
NicolaiOkkels <nico902t@stud.kea.dk>
2020-11-04 10:22:10 +0100

    added deleted getMapping to myController again

--- src/main/java/com/example/demo/controllers/MyController.java
+++ src/main/java/com/example/demo/controllers/MyController.java
@@ -1,7 +1,33 @@
 package com.example.demo.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MyController {
+
+    @GetMapping("/")
+    public String login(){
+        return "index";
+    }
+
+    @GetMapping("/matches")
+    public String matches(){
+        return "matches";
+    }
+
+    @GetMapping("/udforsk")
+    public String explore(){
+        return "udforsk";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/profil")
+    public String profile(){
+        return "profil";
+    }
 }
