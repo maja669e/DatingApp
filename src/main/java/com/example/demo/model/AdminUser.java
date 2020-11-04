c86f2d4c0d96cac995517f68c688e42749d6fbac
Pukkieboy <pukkie93@gmail.com>
2020-11-04 13:54:05 +0100

    Hej

--- src/main/java/com/example/demo/model/AdminUser.java
+++ src/main/java/com/example/demo/model/AdminUser.java
@@ -1,5 +1,8 @@
 package com.example.demo.model;
 
-public class AdminUser {
+public class AdminUser extends BasicUser {
 
+    public AdminUser(String userName, String password) {
+        super(userName, password);
+    }
 }
